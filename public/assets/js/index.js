5ae25467ba13cf4d4f74ea91d7c6dd5a0f0027ad
Nunes <nunestech2@gmail.com>
2024-08-02 17:40:07 -0300

    Commit funcional

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,12 +1,15 @@
 function enableToggleIconMod() {
-    const iconMoon = document.querySelectorAll(".toggle-visibily")[0]
-    const iconSun = document.querySelectorAll(".toggle-visibily")[1]
 
-    if( iconMoon && iconSun){
-        iconMoon.toggleAttribute("hidden")
-        iconSun.toggleAttribute("hidden")
-    }
+    document.querySelectorAll(".toggle-visibily").forEach( item => {
+        item.classList.toggle("hidden")
+    })
+
+    document.body.classList.toggle("night")
 
 }
 
-document.querySelector(".li_topo.icon_mode").addEventListener("click", enableToggleIconMod)
+function setClickListenerOnIconMenu() {
+    document.querySelector(".li_topo.icon_mode").addEventListener("click", enableToggleIconMod)
+}
+
+setClickListenerOnIconMenu()
